a3bd23d074cbaf5bc77d61e136eafb63afe34ec7
Felipe <fellipefreiire3@gmail.com>
2021-11-17 13:44:56 -0300

    useMemo

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import { ProductItem } from "./ProductItem"
 
 interface SearcHResultsProps {
@@ -9,8 +10,16 @@ interface SearcHResultsProps {
 }
 
 export function SearchResults({ results }: SearcHResultsProps) {
+  const totalPrice = useMemo(() => {
+    return results.reduce((total, product) => {
+      return total + product.price
+    }, 0)
+  }, [results])
+
   return (
     <div>
+      <h2>{totalPrice}</h2>
+
       {results.map(product => {
         return (
           <ProductItem key={product.id} product={product} />
