50b67268e006e464ad30f1231acf79159ab0115b
Felipe <fellipefreiire3@gmail.com>
2021-11-17 15:38:21 -0300

    virtualization

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react'
+import { List, ListRowRenderer } from 'react-virtualized'
 import { ProductItem } from "./ProductItem"
 
 interface SearcHResultsProps {
@@ -13,16 +13,35 @@ interface SearcHResultsProps {
 }
 
 export function SearchResults({ results, onAddToWishList, totalPrice }: SearcHResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          onAddToWishList={onAddToWishList}
+        />
+      </div>
+    )
+  }
 
   return (
     <div>
       <h2>{totalPrice}</h2>
 
-      {results.map(product => {
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
+
+      {/* {results.map(product => {
         return (
           <ProductItem key={product.id} product={product} onAddToWishList={onAddToWishList} />
         )
-      })}
+      })} */}
     </div>
   )
 }
