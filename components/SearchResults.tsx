4f457a3690065dc5f61da1dcf6a6d91d01b00678
Felipe <fellipefreiire3@gmail.com>
2021-11-17 14:06:14 -0300

    data formatting

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -5,17 +5,14 @@ interface SearcHResultsProps {
   results: Array<{
     id: number
     price: number
+    priceFormatted: string
     title: string
   }>
   onAddToWishList: (id: number) => void
+  totalPrice: number
 }
 
-export function SearchResults({ results, onAddToWishList }: SearcHResultsProps) {
-  const totalPrice = useMemo(() => {
-    return results.reduce((total, product) => {
-      return total + product.price
-    }, 0)
-  }, [results])
+export function SearchResults({ results, onAddToWishList, totalPrice }: SearcHResultsProps) {
 
   return (
     <div>
